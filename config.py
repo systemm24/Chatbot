b0bc0a09a9dfdee2bf4c95d80070262019ab8a3b
systemm24 <163449633+systemm24@users.noreply.github.com>
2024-03-15 13:08:33 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "24509589"
+# -------------------------------------------------------------
+API_HASH = "717cf21d94c4934bcbe1eaa1ad86ae75"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = "6358262816"
+SUPPORT_GRP = "BABYBOTSX"
+UPDATE_CHNL = "BABYUPDATE"
+OWNER_USERNAME = "VM_COOL"
+
